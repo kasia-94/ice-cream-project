81d833546243c12a2490733de9d585dd2af4f7c0
Butsailo <brodubut1982@gmail.com>
2022-06-11 17:35:00 +0300

    header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,4 @@
-(() => {
+/*(() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -26,4 +26,21 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+})();*/
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".js-open-menu"),
+    closeMenuBtn: document.querySelector(".js-close-menu"),
+    menu: document.querySelector(".js-menu-container"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
 })();

52aa9a2b772734c33c022b8fe978dba533105b2e
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-06-11 23:07:10 +0300

    reviews swiper flip effect

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,19 @@
 const swiper = new Swiper('.swiper', {
+
   // Optional parameters
+    autoplay: {
+   delay: 2500,
+    },
+//     effect: 'fade',
+//   fadeEffect: {
+//     crossFade: false
+//   },
+
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: false
+  },
+       
   direction: 'horizontal',
   loop: true,
 

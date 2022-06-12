802a305e989428cbc84ae81634b9a7086d13a7fa
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-06-12 21:42:52 +0300

    Adds contacts section

---
+++ src/js/modal-contacts-franchise.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[franchise-modal-open]"),
+    closeModalBtn: document.querySelector("[franchise-modal-close]"),
+    modal: document.querySelector("[franchise-modal]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();

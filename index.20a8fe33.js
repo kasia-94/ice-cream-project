67ebd9a118e284bd708fedeea6045f446db65ede
kasia-94 <99141117+kasia-94@users.noreply.github.com>
2022-06-12 06:53:57 +0000

    Deploying to gh-pages from @ kasia-94/ice-cream-project@d76671d52e76c8a9b233b3951f8953c273ddffa3 🚀

---
+++ index.20a8fe33.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{autoplay:{delay:2500},effect:"flip",flipEffect:{slideShadows:!1},direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.20a8fe33.js.map

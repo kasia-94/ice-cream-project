5b1d9c62ea6c7d0687af21346116f567aaaff576
Oleksandr Veremchuk <verem4uk@hotmail.com>
2022-06-14 00:01:32 +0300

    Додав позиціонування та відкриття модалки з меню

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnMenu: document.querySelector("[data-open-modal-menu]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    menu: document.querySelector(".js-menu-container"),
+    body: document.querySelector("body"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnMenu.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    refs.menu.classList.toggle("is-hidden");
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();

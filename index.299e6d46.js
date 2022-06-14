05977fa3f37d31d8777b51309b832d16098d2b26
kasia-94 <99141117+kasia-94@users.noreply.github.com>
2022-06-14 19:58:40 +0000

    Deploying to gh-pages from @ kasia-94/ice-cream-project@168cfa5c96d9b75fbe3ce18dccb745746864eed6 🚀

--- index.299e6d46.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open-about]"),closeModalBtn:document.querySelector("[data-modal-close-about]"),modal:document.querySelector("[data-modal-about]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.299e6d46.js.map
